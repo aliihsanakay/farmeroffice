0b9cc84441644257f0c509274cb43f6e409a182b
Ali ihsan Akay <alihsan.123@hotmail.com>
2019-07-02 08:48:21 +0300

    add angular js and create home controller

---
+++ FarmerOffice.Web/wwwroot/js/angularApp.js
@@ -0,0 +1,1 @@
+﻿var farmerOfficeApp = angular.module('farmerOfficeApp', []);
---
+++ FarmerOffice.Web/wwwroot/js/pages/home.js
@@ -0,0 +1,18 @@
+﻿
+farmerOfficeApp.controller('homeController', ['$scope', '$http', function ($scope, $http) {
+
+
+
+
+}]).directive('onFinishRender', function ($timeout) {
+    return {
+        restrict: 'A',
+        link: function (scope, element, attr) {
+            if (scope.$last === true) {
+                $timeout(function () {
+                    scope.$emit(attr.onFinishRender);
+                });
+            }
+        }
+    };
+});
